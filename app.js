f51bc717dac74bb9f99485c571c4b3fac296c105
rominabaciocchi <rominabaciocchi@gamil.com>
2021-07-31 18:54:00 -0300

    trabajando en el login

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+
+app.listen(3000,() => {
+    console.log("La app esta funcionando en la url http://localhost:3000")});
+
 app.get("/",(req,res) => {
     res.send("Hola")})  
 
@@ -9,7 +13,16 @@ app.get('/home', (request,response)=>{
         response.sendFile(path.join(__dirname,'views/home.html'))
     })
 
-app.listen(3000,() => {
-console.log("La app esta funcionando en la url http://localhost:3000")});
+app.get('/register', function(req,res){
+        res.sendFile(path.resolve(__dirname, 'views/register.html'))
+        });
+    
+app.get('/login', function(req,res){
+        res.sendFile(path.resolve(__dirname, 'views/login.html'))
+        });  
+
+
+
+
 
 app.use (express.static('public')) 
