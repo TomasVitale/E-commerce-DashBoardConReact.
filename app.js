84a0220f2daa539b5693175be089a43a65813c24
rominabaciocchi <rominabaciocchi@gamil.com>
2021-08-02 00:58:11 -0300

    agrego retro.md completo register

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ const path = require("path");
 app.listen(3000,() => {
     console.log("La app esta funcionando en la url http://localhost:3000")});
 
-app.get("/",(req,res) => {
-    res.send("Hola")})  
+app.get("/",(request,response)=>{
+        response.sendFile(path.join(__dirname,'views/home.html'))
+    }); 
 
 app.get('/home', (request,response)=>{
         response.sendFile(path.join(__dirname,'views/home.html'))
-    })
+    });
 
 app.get('/detalleproducto', function(req,res){
         res.sendFile(path.resolve(__dirname, 'views/detalleproducto.html'))
