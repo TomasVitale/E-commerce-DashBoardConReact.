142cb3ef2e7d00a02ce0466d77b4f7115682fa07
rominabaciocchi <rominabaciocchi@gamil.com>
2021-09-12 18:05:07 -0300

    intento de registro y login

--- controllers/maincontroller.js
+++ controllers/maincontroller.js
@@ -1,18 +1,42 @@
+const {validationResult} = require ('express-validator');
+//const bcrypt = require ('bcrypt')
+const User = require ('../models/User')
+
 let mainController= {
-    'home':function (req,res){
-          res.render("")
+    home:function (req,res){
+          res.render("home")
       },
-    'detalleproducto':function (req,res){
-        res.render("")
+    detalleproducto:function (req,res){
+        res.render("detalleproducto")
     },
-    'login':function (req,res){
-        res.render("")
+    login:function (req,res){
+        res.render("login")
     },
-    'register':function (req,res){
-        res.render("")
-    }
+    
+    register:function (req,res){
+        res.render("register")
+    },
+
+    processRegister: function (req,res){
+        const resultValidation = validationResult (req);
+    
+    if (resultValidation.errors.length > 0){
+        return res.render("register", {
+            errors: resultValidation.mapped()
+
+        })
+      }
+
+      /*let userToCreate = {
+          ...req.body,
+          avatar: req.file.filename
+      }*/
+      User.create (req.body);
+      return res.send ('ok, se guardo el usuario')
+    },
+    
   }
 
-  res.render('mainList', {'main':main});
+
   
   module.exports = mainController;
