09818631e7f6c93b3e053f02f9a02bc48a484089
rominabaciocchi <rominabaciocchi@gamil.com>
2021-10-21 21:04:52 -0300

    agrego avatar al register

--- routers/main.js
+++ routers/main.js
@@ -8,13 +8,30 @@ const validations = [
     body("nombre").notEmpty().withMessage ('Tienes que escribir un nombre y apellido'),
     body("email").notEmpty().withMessage ('Tienes que escribir un correo electronico').bail()
     .isEmail().withMessage ('Debes escribir un formato de email valido'),
-    body("telefono").notEmpty().withMessage ('Tienes que escribir un nombre y apellido'),
+    body("telefono").notEmpty().withMessage ('Tienes que escribir un numero de telefono'),
     body("password").notEmpty().withMessage('Tienes que escribir una contraseña'),
+    body ('avatar').custom((value, {req}) =>{
+        let file = req.file;
+        let acceptedExtensions = [ '.jpg' , '.png' , '.gif'];
+
+        
+        if (!file){
+            throw new Error ('Tienes que subir una imagen');
+        } else {
+        let fileExtension = path.extname(file.originalname);
+        if (!acceptedExtensions.includes(fileExtension)){
+            throw new Error (`Las extensiones de archivo permitidas son ${acceptedExtensions.join(', ' )}`);
+
+        }
+    }
+
+        return true;
+    })
 ]
 
 const storage = multer.diskStorage ({
     destination: (req, file, cb)=>{
-       cb(null, './public/images/avatar');
+       cb(null, './public/images/avatars');
     },
     filename: (req, file, cb)=>{
         let fileName = `${Date.now()}_img${path.extname(file.originalname)}`;  
@@ -30,7 +47,7 @@ router.get('/',mainController.home);
 router.get('/home',mainController.home);
 router.get('/detalleproducto',mainController.detalleproducto);
 router.get('/register',mainController.register);
-router.post('/register', validations, mainController.processRegister);
+router.post('/register',uploadFile.single ("avatar"), validations, mainController.processRegister);
 router.get('/login',mainController.login);
 router.post('/login',mainController.processLogin);
 
