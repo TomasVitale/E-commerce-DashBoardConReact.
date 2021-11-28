7615c70360e033948eeb3959cda0996ee8210a10
rominabaciocchi <rominabaciocchi@gamil.com>
2021-11-28 19:46:21 -0300

    agrego mejoras

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,24 +1,8 @@
 const fs = require("fs");
 const path = require("path");
-const db = require('../database/models')
-//llamar a todos los productos del Json
-function allProducts(){
-  
-  let productsJson= fs.readFileSync(path.join(__dirname, "../data/products.json"))
+const db = require('../database/models');
+const { findAll } = require("../models/User");
 
-  
-  let productos = JSON.parse(productsJson)
-  return productos
-}
-
-function writeJson(array){
-  
-  let arrayJson = JSON.stringify(array);
-  
-  
-  return fs.writeFileSync(path.join(__dirname, "../data/products.json"), arrayJson);
-
-}
 
 const productController = {
 
@@ -37,7 +21,7 @@ const productController = {
       category_id: req.body.category,
       color:req.body.color,
       description: req.body.description,
-      image: "prueba",
+      image: req.body.image,
       measure: req.body.measure,
     }
     db.Product.create(product).then(_=> 
@@ -47,15 +31,14 @@ const productController = {
     },
     
 
-   detail:(req, res) => {
-    let products = allProducts ();
-    let productoEncontrado= products.find(function(producto){
-      return producto.id == req.params.id;
-    })
+    detail: async (req,res) =>{
+      let productos = await db.Product.findAll(); 
 
-    res.render ("detail", {producto : productoEncontrado})
-  
-},
+     let producto = await db.Product.findByPk(req.params.id);
+
+     res.render('detail',{ producto:producto, productos:productos});
+
+    },
 
 create: function (req, res){
 
@@ -65,26 +48,7 @@ create: function (req, res){
 
   })
 
- /* let products = allProducts ();
-
-    let nuevoProducto = {
-      id: products.length + 1 ,
-      name: req.body.name ,
-      price: req.body.price,
-      category: req.body.category ,
-      color: req.body.color,
-      description : req.body.description,
-      measure : req.body.measure,
-      image : req.body.image
-    }
-    
-    let productosActualizados = [...products, nuevoProducto]
-
-    
-    writeJson(productosActualizados);
-
-    
-    res.redirect("/products/list");*/
+ 
 },
 
 
@@ -107,7 +71,7 @@ update: async (req, res)=> {
     category_id: req.body.category,
     color:req.body.color,
     description: req.body.description,
-    image: "prueba",
+    image: req.body.image,
     measure: req.body.measure,
 },
   {
@@ -117,38 +81,16 @@ update: async (req, res)=> {
    });
    res.redirect('/products/list');
 
-  // let productosActualizados = products.map(function(producto){
-  //   if(producto.id == req.params.id){
-  //     producto.name = req.body.name 
-  //     producto.price = req.body.price
-  //     producto.category = req.body.category 
-  //     producto.color = req.body.color
-  //     producto.description = req.body.description
-  //     producto.measure = req.body.measure
-  //     producto.image = req.body.image
-  //   }
-  //   return producto
-  // })
   
- 
-  writeJson(productosActualizados);
-  
-
- res.redirect("/products/detail/"+req.params.id)
 
 },
 
 destroy: (req, res) =>{
-  let products = allProducts ();
-
-
-  let dataNueva = products.filter(function(producto){
-    return producto.id != req.params.id
-  })
-
-  writeJson(dataNueva);
-
-  
+  db.Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  });
   res.redirect("/products/list");
 }};
 
--- routers/products.js
+++ routers/products.js
@@ -5,9 +5,7 @@ const mainController = require('../controllers/maincontroller.js')
 
 
 router.get("/list", productsController.list);
-//router.get("/create", productsController.create);
 router.get("/detail/:id", productsController.detail);
-//router.post("/create", productsController.store);
 router.get("/edit/:id", productsController.edit);
 router.put("/edit/:id", productsController.update);
 router.delete("/delete/:id" , productsController.destroy);
